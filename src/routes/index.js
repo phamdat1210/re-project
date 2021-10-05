572ed392d58118d21d7b02e8c360b8d773473ab1
phamdat1210 <62377341+phamdat1210@users.noreply.github.com>
2021-10-06 00:28:17 +0700

    re-deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 // import PrivateRoute from './auth-route';
 import { AuthContext } from '../context/auth';
 import HomeScreen from '../pages/HomeScreen';
